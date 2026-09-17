7b58a51e40d040d7e7db87acf231895175b3597d
Santiago Montes Jiménez <santimmovil@gmail.com>
2026-09-17 20:25:03 +0200

    Día 2 final

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -4,9 +4,9 @@
 from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\input\gradient_alt100_act150" # small scene
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-ISM\myoutput"
+auxdir = "C:/Users/santi/Desktop/MSTR-2/PODT/gitt/gitttt/auxiliary"
+indir = "C:/Users/santi/Desktop/MSTR-2/PODT/EODP_TER_2021/EODP-TS-ISM/input/gradient_alt100_act150" # small scene
+outdir = "C:/Users/santi/Desktop/MSTR-2/PODT/EODP_TER_2021/EODP-TS-ISM/output_santi"
 
 # Initialise the ISM
 myIsm = ism(auxdir, indir, outdir)
--- ism/src/opticalPhase.py
+++ ism/src/opticalPhase.py
@@ -93,6 +93,8 @@ class opticalPhase(initIsm):
         :return: TOA image in irradiances [mW/m2]
         """
         # TODO
+        #esto es nuestro
+        toa = Tr * toa * (np.pi / 4) * ((D/f)**2)
         return toa
 
 
@@ -115,6 +117,27 @@ class opticalPhase(initIsm):
         :return: TOA image 2D in radiances [mW/m2]
         """
         # TODO
+        isrf, wv_isrf = readIsrf(self.auxdir + '/' + self.ismConfig.isrffile, band)
+
+        #0 inicialitez output
+        toa = np.zeros((sgm_toa.shape[0], sgm_toa.shape[1]))
+        # 1. normalis ISRF
+        isrf = isrf/np.sum(isrf)  #homemade
+
+        # 2. convert ISRF wavelengths to nanometers
+        wv_isrf = wv_isrf * 1000
+
+        # 3. creating interpoliting of the ISRF - interp ISRF to the SGM
+        # cs = interp1d(wv_isrf, isrf, fill_value=(0, 0), bounds_error=False)
+        # interp_isrf = cs(sgm_wv)
+
+        for ialt in range(sgm_toa.shape[0]):
+            for iact in range(sgm_toa.shape[1]):
+                cs = interp1d(sgm_wv, sgm_toa[ialt, iact,:], fill_value=(0,0), bounds_error=False)
+                sgm_inter = cs(wv_isrf)
+                toa[ialt, iact]=np.sum(sgm_inter*isrf)
+
+        # sum of the normalized isrf is 1
         return toa
 
 
