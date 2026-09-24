b14123965db972d916f0f07888b5adedf55351e1
Santiago Montes Jiménez <santimmovil@gmail.com>
2026-09-24 18:44:33 +0200

    Clase 3 - 1

--- config/l1bConfig.py
+++ config/l1bConfig.py
@@ -8,7 +8,11 @@ class l1bConfig:
     def __init__(self):
 
         # Flags to enable or disable the equalization
-        self.do_equalization = False   #segunda clase
+        self.do_equalization = False
+
+
+
+        #segunda clase
 
         # Auxiliary inputs (relative paths to the root folder)
         #--------------------------------------------------------------------------------
--- ism/src/mtf.py
+++ ism/src/mtf.py
@@ -92,6 +92,21 @@ class mtf:
         :return fnAlt: 1D normalised frequencies 2D ALT (f/(1/w))
         """
         #TODO
+        fstepAlt = 1 / nlines / w
+        fstepAct = 1 / ncolumns / w
+        eps = 1e-6
+        fAlt = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAlt) #alone track
+        fAct = np.arange(-1 / (2 * w), 1 / (2 * w) - eps, fstepAct) #across track
+
+        [fAltxx, fActxx] = np.meshgrid(fAlt, fAct, indexing='ij')
+        f2D = np.sqrt(fAltxx * fAltxx + fActxx * fActxx)
+
+        fn2D = f2D /(1/w)
+        fc = D / (lambd * focal)
+        fr2D = f2D / (fc)
+        fnAct = fAct / (1/w)
+        fnAlt = fAlt / (1/w)
+
         return fn2D, fr2D, fnAct, fnAlt
 
     def mtfDiffract(self,fr2D):
